0f955ae301905b0d70d375d030d1b0021007f677
Margaret <mungaimargaretn@gmail.com>
2024-03-20 14:30:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 =2;
+const num2 = 31;
+let multiply =num1 * num2;
+
+let random = Math.ceil(Math.random()*10);
+
+const num3 =8;
+const num4 =2;
+let mod = num3/num4;
+
+let max = Math.max(1,2,5,16,20);
